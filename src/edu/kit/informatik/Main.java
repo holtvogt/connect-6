64ea07d832a1002f4d2f0b73079e5cab3fbdc928
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2019-02-15 21:20:56 +0100

    Update command line arguments passing

--- src/edu/kit/informatik/Main.java
+++ src/edu/kit/informatik/Main.java
@@ -1,17 +1,27 @@
 package edu.kit.informatik;
 
 import edu.kit.informatik.game.ConnectSix;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
- * 
+ *
  * @author Björn Holtvogt
  *
  */
 public final class Main {
 
     /**
+     * Reads text from the "standard" input stream, buffering characters so as to provide for the efficient reading
+     * of characters, arrays, and lines. This stream is already open and ready to supply input data and corresponds
+     * to keyboard input.
+     */
+    private static final BufferedReader IN = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
      * Private constructor to avoid object generation.
-     * 
+     *
      * @deprecated Utility-class constructor.
      */
     @Deprecated
@@ -22,7 +32,7 @@ public final class Main {
 
     /**
      * This is the program entry method main.
-     * 
+     *
      * @param args
      *            Array of strings of the given command line arguments.
      */
@@ -33,24 +43,26 @@ public final class Main {
         if (connectSix.entryCheck(args)) {
             do {
                 try {
-                    command = Command.matchingCommand(Terminal.readLine(), connectSix);
+                    command = Command.matchingCommand(IN.readLine(), connectSix);
                 } catch (InvalidInputException invalidInputException) {
-                    Terminal.printError(invalidInputException.getMessage());
+                    System.out.println("Error, " + invalidInputException.getMessage());
                 } catch (NumberFormatException numberFormatException) {
-                    Terminal.printError("input isn't equal to an integer.");
-                }
+                    System.out.println("Error, input isn't equal to an integer.");
+                } catch (IOException ioException) {
+					throw new RuntimeException(ioException);
+				}
             } while (command == null || command.isRunning());
         }
     }
 
     /**
      * Communication of the program with the user/s.
-     * 
+     *
      * @param text
      *            Answer of an command input to the user/s.
      */
     public static void textOutput(String text) {
 
-        Terminal.printLine(text);
+        System.out.println(text);
     }
 }
