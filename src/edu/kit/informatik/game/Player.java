a03e8ff26dee161fc95c9900413c2fc0725495e4
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2021-09-01 12:42:19 +0200

    Add final modifier and apply format changes

--- src/edu/kit/informatik/game/Player.java
+++ src/edu/kit/informatik/game/Player.java
@@ -1,99 +1,91 @@
 package edu.kit.informatik.game;
 
-import java.util.Map;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class represents the players in the game.
- * 
- * @author Björn Holtvogt
- *
  */
 public enum Player {
 
-	/**
-	 * Player 1.
-	 */
-	P1(0),
+    /**
+     * Player 1.
+     */
+    P1(0),
 
-	/**
-	 * Player 2.
-	 */
-	P2(1),
+    /**
+     * Player 2.
+     */
+    P2(1),
 
-	/**
-	 * Player 3.
-	 */
-	P3(2),
+    /**
+     * Player 3.
+     */
+    P3(2),
 
-	/**
-	 * Player 4.
-	 */
-	P4(3);
+    /**
+     * Player 4.
+     */
+    P4(3);
 
-	private static Map<Integer, Player> playerMap = new HashMap<>();
-	private int playerID;
+    private static final Map<Integer, Player> PLAYER_MAP = new HashMap<>();
+    private final int playerID;
 
-	/**
-	 * Creates a player with his player ID.
-	 * 
-	 * @param playerID Player ID.
-	 */
-	private Player(final int playerID) {
+    /**
+     * Creates a player with his player ID.
+     *
+     * @param playerID Player ID.
+     */
+    Player(final int playerID) {
+        this.playerID = playerID;
+    }
 
-		this.playerID = playerID;
-	}
+    static {
+        for (Player player : Player.values()) {
+            PLAYER_MAP.put(player.getPlayerID(), player);
+        }
+    }
 
-	static {
+    /**
+     * Returns the player ID.
+     *
+     * @return The player ID.
+     */
+    private int getPlayerID() {
+        return playerID;
+    }
 
-		for (Player player : Player.values()) {
-			playerMap.put(player.getPlayerID(), player);
-		}
-	}
+    /**
+     * Returns the player by his player ID.
+     *
+     * @param id Player ID.
+     * @return The Player.
+     */
+    private Player getPlayerByID(final int id) {
+        return PLAYER_MAP.get(id);
+    }
 
-	/**
-	 * Returns the player ID.
-	 * 
-	 * @return The player ID.
-	 */
-	private int getPlayerID() {
+    /**
+     * Returns the maximum amount of players in this game.
+     *
+     * @return The maximum amount of players in this game.
+     */
+    public int getMaxAmountOfPlayers() {
+        return PLAYER_MAP.size();
+    }
 
-		return playerID;
-	}
-
-	/**
-	 * Returns the player by his player ID.
-	 * 
-	 * @param id Player ID.
-	 * @return The Player.
-	 */
-	private Player getPlayerByID(final int id) {
-
-		return playerMap.get(id);
-	}
-
-	/**
-	 * Returns the maximum amount of players in this game.
-	 * 
-	 * @return The maximum amount of players in this game.
-	 */
-	public int getMaxAmountOfPlayers() {
-
-		return playerMap.size();
-	}
-
-	/**
-	 * Returns the next player.
-	 * 
-	 * @param playerAmount The maximum amount of players in this game.
-	 * @return The next player.
-	 */
-	public Player getNextPlayer(final int playerAmount) {
-
-		if (playerAmount != playerMap.size()) {
-			return getPlayerByID((playerID + 1) % playerAmount);
-		} else {
-			return getPlayerByID((playerID + 1) % playerMap.size());
-		}
-	}
+    /**
+     * Returns the next player.
+     *
+     * @param playerAmount The maximum amount of players in this game.
+     * @return The next player.
+     */
+    public Player getNextPlayer(final int playerAmount) {
+        if (playerAmount != PLAYER_MAP.size()) {
+            return getPlayerByID((playerID + 1) % playerAmount);
+        } else {
+            return getPlayerByID((playerID + 1) % PLAYER_MAP.size());
+        }
+    }
 }
