f016a4cab966b399c321111cb3736ad71e077195
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2021-09-01 12:41:38 +0200

    Remove output method and apply format changes

--- src/edu/kit/informatik/Main.java
+++ src/edu/kit/informatik/Main.java
@@ -1,59 +1,51 @@
 package edu.kit.informatik;
 
-import edu.kit.informatik.exceptions.InvalidInputException;
 import edu.kit.informatik.game.ConnectSix;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
  * The main class is the entry point of the connect 6 game application.
- *
- * @author Björn Holtvogt
- *
  */
 public final class Main {
 
-	/**
-	 * Reads text from the "standard" input stream, buffering characters so as to
-	 * provide for the efficient reading of characters, arrays, and lines. This
-	 * stream is already open and ready to supply input data and corresponds to
-	 * keyboard input.
-	 */
-	private static final BufferedReader IN = new BufferedReader(new InputStreamReader(System.in));
+    /**
+     * Reads text from the "standard" input stream, buffering characters to
+     * provide for the efficient reading of characters, arrays, and lines. This
+     * stream is already open and ready to supply input data and corresponds to
+     * keyboard input.
+     */
+    private static final BufferedReader IN = new BufferedReader(new InputStreamReader(System.in));
 
-	/**
-	 * Private constructor to avoid object generation.
-	 *
-	 * @deprecated Utility-class constructor.
-	 */
-	@Deprecated
-	private Main() {
+    /**
+     * Private constructor to avoid object generation.
+     */
+    private Main() {
+        throw new AssertionError("Utility class constructor.");
+    }
 
-		throw new AssertionError("Utility class constructor.");
-	}
-
-	/**
-	 * This is the program entry method main.
-	 *
-	 * @param args Array of strings of the given command line arguments.
-	 */
-	public static void main(final String[] args) {
-
-		ConnectSix connectSix = new ConnectSix();
-		Command command = null;
-		if (connectSix.entryCheck(args)) {
-			do {
-				try {
-					command = Command.matchingCommand(IN.readLine(), connectSix);
-				} catch (InvalidInputException invalidInputException) {
-					connectSix.output("Error, " + invalidInputException.getMessage());
-				} catch (NumberFormatException numberFormatException) {
-					connectSix.output("Error, input isn't equal to an integer.");
-				} catch (IOException ioException) {
-					throw new RuntimeException(ioException);
-				}
-			} while (command == null || command.isRunning());
-		}
-	}
+    /**
+     * This is the program entry method main.
+     *
+     * @param args Array of strings of the given command line arguments.
+     */
+    public static void main(final String[] args) {
+        ConnectSix connectSix = new ConnectSix();
+        Command command = null;
+        if (connectSix.entryCheck(args)) {
+            do {
+                try {
+                    command = Command.matchingCommand(IN.readLine(), connectSix);
+                } catch (InvalidInputException invalidInputException) {
+                    System.out.println("Error, " + invalidInputException.getMessage());
+                } catch (NumberFormatException numberFormatException) {
+                    System.out.println("Error, input isn't equal to an integer.");
+                } catch (IOException ioException) {
+                    throw new RuntimeException(ioException);
+                }
+            } while (command == null || command.isRunning());
+        }
+    }
 }
