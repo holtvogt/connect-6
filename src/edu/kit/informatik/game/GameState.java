1678d92e1bef41207918e6be781b56bf4b377f13
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2021-09-01 12:42:31 +0200

    Apply format changes

--- src/edu/kit/informatik/game/GameState.java
+++ src/edu/kit/informatik/game/GameState.java
@@ -2,24 +2,21 @@ package edu.kit.informatik.game;
 
 /**
  * This class implements the current game state.
- * 
- * @author Björn Holtvogt
- *
  */
 public enum GameState {
 
-	/**
-	 * Game is running - state.
-	 */
-	RUNNING,
+    /**
+     * Game is running - state.
+     */
+    RUNNING,
 
-	/**
-	 * Game ended in a draw - state.
-	 */
-	DRAW,
+    /**
+     * Game ended in a draw - state.
+     */
+    DRAW,
 
-	/**
-	 * Game ended with a win - state.
-	 */
-	WON;
+    /**
+     * Game ended with a win - state.
+     */
+    WON
 }
